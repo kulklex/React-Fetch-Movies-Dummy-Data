5665aa57f34f073e89761083df6d0c74c0dfbdd3
Kulklex <kulklex@gmail.com>
2022-05-29 23:12:50 +0100

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.css"
+
+export default function Navbar() {
+  return (
+    <div>
+        <div className='navbarHome'>
+          <h1 className='moviesApp'>Movies App</h1>
+          <h4  className='favourites'>Favourites</h4>
+        </div>
+    </div>
+  )
+}
